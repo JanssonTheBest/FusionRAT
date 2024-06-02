347ac9d35db5c45eae57a5d893b55d21c8ca8f1b
JanssonTheBest <ertyrix@gmail.com>
2024-06-02 03:03:16 +0200

    fix ssasasa

--- Client/Settings.cs
+++ Client/Settings.cs
@@ -10,7 +10,7 @@ namespace Client
     public static class Settings
     {
 #if DEBUG
-        public static string ip = "84.216.179.171";
+        public static string ip = "127.0.0.1";
         public static int port = 2332;
         public static string version = "debug";
 #else
