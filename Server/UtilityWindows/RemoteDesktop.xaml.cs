565d2db6f1205b0d828339d4ff6cb1e8ccb80b0d
JanssonTheBest <ertyrix@gmail.com>
2024-06-02 20:38:06 +0200

    rdddppp

--- Server/UtilityWindows/RemoteDesktop.xaml.cs
+++ Server/UtilityWindows/RemoteDesktop.xaml.cs
@@ -6,6 +6,8 @@ using Common.DTOs.MessagePack;
 using System.Windows.Media.Imaging;
 using Server.CoreServerFunctionality;
 using Server.UtilityWindows.Interface;
+using System.Windows.Controls;
+using System.Drawing;
 
 namespace Server.UtilityWindows
 {
@@ -31,12 +33,12 @@ namespace Server.UtilityWindows
         private async void HandlePacket(object? sender, EventArgs e)
         {
             var dto = (RemoteDesktopDTO)sender;
-            if(dto.Screen is not null)
+            if (dto.Screen is not null)
             {
-
+                await Application.Current.Dispatcher.InvokeAsync(() => screens.Items.Add(string.Join("|", dto.Screen)));
                 return;
             }
-           await DisplayFrame(dto.Frame);
+            await DisplayFrame(dto.Frame);
         }
 
         private async Task DisplayFrame(byte[] frameBytes)
@@ -57,6 +59,24 @@ namespace Server.UtilityWindows
             });
         }
 
+        private byte[] BitmapToJpegBytes(byte[] byteArray)
+        {
+            using (var stream = new MemoryStream(byteArray))
+            {
+                Bitmap bmp = new Bitmap(stream);
+
+                // Create a new memory stream to save the JPEG image
+                using (MemoryStream jpegStream = new MemoryStream())
+                {
+                    // Save the bitmap as JPEG to the memory stream
+                    bmp.Save(jpegStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                    // Return the byte array of the JPEG image
+                    return jpegStream.ToArray();
+                }
+            }
+        }
+
         private ImageSource ByteArrayToImageSource(byte[] byteArray)
         {
             using (var stream = new MemoryStream(byteArray))
@@ -70,9 +90,29 @@ namespace Server.UtilityWindows
                 return bitmap;
             }
         }
+        public BitmapSource ByteArrayToBitmapSource(byte[] bitmapBytes, int width, int height)
+        {
+            var bitmapSource = BitmapSource.Create(
+                width, height, 96, 96, // dpiX and dpiY
+                System.Windows.Media.PixelFormats.Bgra32, // Pixel format
+                null, // Palette
+                bitmapBytes, // Bitmap data
+                width * 4 // Stride (width * 4 bytes per pixel)
+            );
+
+            return bitmapSource;
+        }
 
         private void icon_Loaded(object sender, RoutedEventArgs e)
         {
+        }
+
+        private async void screens_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            await _serverSession.SendPacketAsync(new RemoteDesktopDTO
+            {
+                Screen = new string[] { "dd" }
+            });
         }
     }
 }
