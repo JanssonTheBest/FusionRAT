e98f605451658e777be3363660085a5d4bdf2db1
DPric <c36177541@gmail.com>
2024-06-04 03:01:59 +0200

    fixat en bättre custom comboBox

--- Server/UI/Pages/SettingsPage/Settings.xaml.cs
+++ Server/UI/Pages/SettingsPage/Settings.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -24,5 +25,11 @@ namespace Server.UI.Pages.SettingsPage
         {
             InitializeComponent();
         }
+
+        private void PopupBorder_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+        }
+
     }
 }
