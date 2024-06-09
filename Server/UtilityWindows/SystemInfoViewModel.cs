15edd4d3714e24d35e6baec5ed350a043f7243ee
Xein <100153223+XeinTDM@users.noreply.github.com>
2024-06-10 01:34:03 +0200

    a lot of changes

---
+++ Server/UtilityWindows/SystemInfoViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Server.UtilityWindows
+{
+    public class SystemInfoViewModel
+    {
+        public ObservableCollection<SystemInfoItem> SystemInfoItems { get; set; }
+
+        public SystemInfoViewModel()
+        {
+            SystemInfoItems = new ObservableCollection<SystemInfoItem>
+            {
+                new SystemInfoItem { Key = "BIOS Version", Value = "1.0.0" },
+                new SystemInfoItem { Key = "System Manufacturer", Value = "Example Manufacturer" },
+                new SystemInfoItem { Key = "System Model", Value = "Example Model" }
+                // Add more items as needed
+            };
+        }
+    }
+
+    public class SystemInfoItem
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
