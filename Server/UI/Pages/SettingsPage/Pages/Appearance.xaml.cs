a03e4c389a885b51c684f11216ab1be02d1a424d
Xein <100153223+XeinTDM@users.noreply.github.com>
2024-06-15 18:37:29 +0200

    Fixed appearnce themes

--- Server/UI/Pages/SettingsPage/Pages/Appearance.xaml.cs
+++ Server/UI/Pages/SettingsPage/Pages/Appearance.xaml.cs
@@ -1,47 +1,46 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows;
 
 namespace Server.UI.Pages.SettingsPage.Pages
 {
     public partial class Appearance : UserControl
     {
-        public ObservableCollection<ThemeColors> Themes { get; set; }
+
+        private readonly List<Theme> _themes =
+        [
+            new() { Primary = "#202020", Secondary = "#272727", Border = "#4B4B4B", Accent = "#FF4081", Text = "#FFFFFF", Selected = "#E91E63" },
+            new() { Primary = "#131313", Secondary = "#000000", Border = "#4B4B4B", Accent = "#1E9BFD", Text = "#FFFFFF", Selected = "#E91E63" },
+            new() { Primary = "#16141C", Secondary = "#2B293D", Border = "#4B4B4B", Accent = "#FF4081", Text = "#FFFFFF", Selected = "#E91E63" },
+            new() { Primary = "#372E45", Secondary = "#453C56", Border = "#4B4B4B", Accent = "#FF4081", Text = "#FFFFFF", Selected = "#E91E63" },
+            new() { Primary = "#FFFFFF", Secondary = "#F5F5F5", Border = "#4B4B4B", Accent = "#FF4081", Text = "#000000", Selected = "#E91E63" },
+            new() { Primary = "#535364", Secondary = "#F5F5F5", Border = "#4B4B4B", Accent = "#FF4081", Text = "#000000", Selected = "#E91E63" },
+            new() { Primary = "#282F54", Secondary = "#F5F5F5", Border = "#4B4B4B", Accent = "#FF4081", Text = "#000000", Selected = "#E91E63" },
+            new() { Primary = "#44348C", Secondary = "#F5F5F5", Border = "#4B4B4B", Accent = "#FF4081", Text = "#000000", Selected = "#E91E63" }
+        ];
 
         public Appearance()
         {
             InitializeComponent();
-            DataContext = this;
-            Themes =
-            [
-                new() { Primary = "#202020", Secondary = "#272727", Border = "#4B4B4B", Accent = "#FF4081", Text = "#FFFFFF", Selected = "#E91E63" },
-                new() { Primary = "#131313", Secondary = "#000000", Border = "#4B4B4B", Accent = "#FF4081", Text = "#FFFFFF", Selected = "#E91E63" },
-                new() { Primary = "#16141C", Secondary = "#2B293D", Border = "#4B4B4B", Accent = "#FF4081", Text = "#FFFFFF", Selected = "#E91E63" },
-                new() { Primary = "#372E45", Secondary = "#453C56", Border = "#4B4B4B", Accent = "#FF4081", Text = "#FFFFFF", Selected = "#E91E63" },
-                new() { Primary = "#FFFFFF", Secondary = "#F5F5F5", Border = "#4B4B4B", Accent = "#FF4081", Text = "#000000", Selected = "#E91E63" },
-                new() { Primary = "#535364", Secondary = "#F5F5F5", Border = "#4B4B4B", Accent = "#FF4081", Text = "#000000", Selected = "#E91E63" },
-                new() { Primary = "#282F54", Secondary = "#F5F5F5", Border = "#4B4B4B", Accent = "#FF4081", Text = "#000000", Selected = "#E91E63" },
-                new() { Primary = "#44348C", Secondary = "#F5F5F5", Border = "#4B4B4B", Accent = "#FF4081", Text = "#000000", Selected = "#E91E63" }
-            ];
         }
 
         private void ThemeRadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            if (sender is RadioButton radioButton && radioButton.Tag is ThemeColors themeColors)
+            if (sender is RadioButton radioButton)
             {
-                ChangeTheme(themeColors);
+                int themeIndex = int.Parse(radioButton.Name.Replace("ThemeRadioButton", "")) - 1;
+                ApplyTheme(_themes[themeIndex]);
             }
         }
 
-        private void ChangeTheme(ThemeColors colors)
+        private void ApplyTheme(Theme theme)
         {
-            Application.Current.Resources["Primary"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(colors.Primary));
-            Application.Current.Resources["Secondary"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(colors.Secondary));
-            Application.Current.Resources["Border"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(colors.Border));
-            Application.Current.Resources["Accent"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(colors.Accent));
-            Application.Current.Resources["Text"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(colors.Text));
-            Application.Current.Resources["Selected"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(colors.Selected));
+            Application.Current.Resources["Primary"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(theme.Primary));
+            Application.Current.Resources["Secondary"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(theme.Secondary));
+            Application.Current.Resources["Border"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(theme.Border));
+            Application.Current.Resources["Accent"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(theme.Accent));
+            Application.Current.Resources["Text"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(theme.Text));
+            Application.Current.Resources["Selected"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(theme.Selected));
         }
 
         public enum themeNames
@@ -56,8 +55,7 @@ namespace Server.UI.Pages.SettingsPage.Pages
             Settings.AppearanceStoryboard.DynamicInvoke(themeNames.SettingsContentVisible);
         }
     }
-
-    public class ThemeColors
+    public class Theme
     {
         public string Primary { get; set; }
         public string Secondary { get; set; }
