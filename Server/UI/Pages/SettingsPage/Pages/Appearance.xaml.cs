dcd0fec0e2e7e98ccc2c2de760804aa57a1ff478
DPric <c36177541@gmail.com>
2024-06-17 01:35:15 +0200

    working shade colors

--- Server/UI/Pages/SettingsPage/Pages/Appearance.xaml.cs
+++ Server/UI/Pages/SettingsPage/Pages/Appearance.xaml.cs
@@ -6,18 +6,22 @@ namespace Server.UI.Pages.SettingsPage.Pages
 {
     public partial class Appearance : UserControl
     {
+        private readonly List<Theme> _themes = new List<Theme>
+        {
+            new() { Primary = "#202020", Secondary = "#272727", Border = "#4B4B4B", Text = "#FFFFFF", Selected = "#E91E63" },
+            new() { Primary = "#0B0B0B", Secondary = "#0F0F0F", Border = "#1B1B1B", Text = "#FFFFFF", Selected = "#E91E63" },
+            new() { Primary = "#16141C", Secondary = "#1A1820", Border = "#4C495B", Text = "#FFFFFF", Selected = "#E91E63" },
+            new() { Primary = "#011623", Secondary = "#041A2D", Border = "#0E2B44", Text = "#FFFFFF", Selected = "#E91E63" },
+            new() { Primary = "#FFFFFF", Secondary = "#F5F5F5", Border = "#4B4B4B", Text = "#000000", Selected = "#E91E63" },
+            new() { Primary = "#535364", Secondary = "#F5F5F5", Border = "#4B4B4B", Text = "#000000", Selected = "#E91E63" },
+            new() { Primary = "#282F54", Secondary = "#F5F5F5", Border = "#4B4B4B", Text = "#000000", Selected = "#E91E63" },
+            new() { Primary = "#44348C", Secondary = "#F5F5F5", Border = "#4B4B4B", Text = "#000000", Selected = "#E91E63" }
+        };
 
-        private readonly List<Theme> _themes =
-        [
-            new() { Primary = "#202020", Secondary = "#272727", Border = "#4B4B4B", Accent = "#1E9BFD", Text = "#FFFFFF", Selected = "#E91E63" },
-            new() { Primary = "#0B0B0B", Secondary = "#0F0F0F", Border = "#1B1B1B", Accent = "#FF0000", Text = "#FFFFFF", Selected = "#E91E63" },
-            new() { Primary = "#16141C", Secondary = "#1A1820", Border = "#4C495B", Accent = "#B7A6FF", Text = "#FFFFFF", Selected = "#E91E63" },
-            new() { Primary = "#011623", Secondary = "#041A2D", Border = "#0E2B44", Accent = "#57BFFF", Text = "#FFFFFF", Selected = "#E91E63" },
-            new() { Primary = "#FFFFFF", Secondary = "#F5F5F5", Border = "#4B4B4B", Accent = "#1E9BFD", Text = "#000000", Selected = "#E91E63" },
-            new() { Primary = "#535364", Secondary = "#F5F5F5", Border = "#4B4B4B", Accent = "#1E9BFD", Text = "#000000", Selected = "#E91E63" },
-            new() { Primary = "#282F54", Secondary = "#F5F5F5", Border = "#4B4B4B", Accent = "#1E9BFD", Text = "#000000", Selected = "#E91E63" },
-            new() { Primary = "#44348C", Secondary = "#F5F5F5", Border = "#4B4B4B", Accent = "#1E9BFD", Text = "#000000", Selected = "#E91E63" }
-        ];
+        private readonly List<string> _shades = new List<string>
+        {
+            "#1E9BFD", "#FF0000", "#B7A6FF", "#936DFF", "#FFEC3D", "#6DFF7C", "#FFFFFF"
+        };
 
         public Appearance()
         {
@@ -38,9 +42,22 @@ namespace Server.UI.Pages.SettingsPage.Pages
             Application.Current.Resources["Primary"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(theme.Primary));
             Application.Current.Resources["Secondary"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(theme.Secondary));
             Application.Current.Resources["Border"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(theme.Border));
-            Application.Current.Resources["Accent"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(theme.Accent));
             Application.Current.Resources["Text"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(theme.Text));
             Application.Current.Resources["Selected"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(theme.Selected));
+        }
+
+        private void ShadeRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender is RadioButton radioButton)
+            {
+                string shade = radioButton.Background.ToString();
+                ApplyAccent(shade);
+            }
+        }
+
+        private void ApplyAccent(string shade)
+        {
+            Application.Current.Resources["Accent"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString(shade));
         }
 
         public enum themeNames
@@ -55,12 +72,12 @@ namespace Server.UI.Pages.SettingsPage.Pages
             Settings.AppearanceStoryboard.DynamicInvoke(themeNames.SettingsContentVisible);
         }
     }
+
     public class Theme
     {
         public string Primary { get; set; }
         public string Secondary { get; set; }
         public string Border { get; set; }
-        public string Accent { get; set; }
         public string Text { get; set; }
         public string Selected { get; set; }
     }
