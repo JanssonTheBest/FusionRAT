23a163df485776afa71e6227d9e626f98a8110db
Xein <100153223+XeinTDM@users.noreply.github.com>
2024-06-16 19:11:16 +0200

    fixed the server input & listen button and also fixed so client row doesn't get unselected when ping reloads the row.

--- Server/UI/Pages/ServerPage/Server.xaml.cs
+++ Server/UI/Pages/ServerPage/Server.xaml.cs
@@ -1,26 +1,10 @@
 ﻿using Server.CoreServerFunctionality;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Security;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Server.UI.Pages.ServerPage
 {
-    /// <summary>
-    /// Interaction logic for Server.xaml
-    /// </summary>
     public partial class Server : UserControl
     {
         public Server()
@@ -28,7 +12,7 @@ namespace Server.UI.Pages.ServerPage
             InitializeComponent();
             listener.OnClientConnected += OnClientConnected;
         }
-        Listener listener = new Listener();
+        Listener listener = new();
         private void portStart_Click(object sender, RoutedEventArgs e)
         {
             listener.AddPortToListener(int.Parse(portInput.Text)).GetAwaiter().GetResult();
