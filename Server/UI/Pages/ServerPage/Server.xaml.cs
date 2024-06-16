ef1b7ab4726126612d36b6d4c9136e4001872097
Xein <100153223+XeinTDM@users.noreply.github.com>
2024-06-16 20:15:43 +0200

    Added so when adding a port it comes up in the datagrid.

--- Server/UI/Pages/ServerPage/Server.xaml.cs
+++ Server/UI/Pages/ServerPage/Server.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Server.CoreServerFunctionality;
+using System.Collections.ObjectModel;
 using System.Net.Sockets;
 using System.Windows;
 using System.Windows.Controls;
@@ -7,15 +8,21 @@ namespace Server.UI.Pages.ServerPage
 {
     public partial class Server : UserControl
     {
+        public ObservableCollection<PortStatus> Ports { get; set; } = new ObservableCollection<PortStatus>();
+
         public Server()
         {
             InitializeComponent();
             listener.OnClientConnected += OnClientConnected;
+            dataGridPorts.ItemsSource = Ports;
         }
+
         Listener listener = new();
         private void portStart_Click(object sender, RoutedEventArgs e)
         {
-            listener.AddPortToListener(int.Parse(portInput.Text)).GetAwaiter().GetResult();
+            int port = int.Parse(portInput.Text);
+            listener.AddPortToListener(port).GetAwaiter().GetResult();
+            Ports.Add(new PortStatus { Port = port, Status = "Listening", Name = "Placeholder" });
         }
 
         private void OnClientConnected(object sender, EventArgs e)
@@ -25,5 +32,12 @@ namespace Server.UI.Pages.ServerPage
                 await ClientHandler.HandleClient((TcpClient)sender);
             });
         }
+    }
+
+    public class PortStatus
+    {
+        public int Port { get; set; }
+        public string Status { get; set; }
+        public string Name { get; set; }
     }
 }
