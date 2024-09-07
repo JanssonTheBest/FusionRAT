406f453243c2794f3c5493d3809f31bc066b6460
yipandyaphack123 <yipandyaphack123@proton.me>
2024-09-07 18:16:40 +0200

    fixed

--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -30,9 +30,9 @@ namespace Server
                 ExpandSidePanel.Begin(this);
             }
 
-            new Notification("Client Disconnected", "Evoq disconnected from server 2332 ", "#2686E6", "pack://application:,,,/UI/Assets/Icons/Info.png", "Transparent", "#1100ACFF").Show();
-            new Notification("Client Disconnected", "Evoq disconnected from server 2332 ", "#E80000", "pack://application:,,,/UI/Assets/Icons/warning.png", "Transparent", "#11FF0000").Show();
-            new Notification("Client Disconnected", "Evoq disconnected from server 2332 Evoq disconnected from server 2332", "#52FF00", "pack://application:,,,/UI/Assets/Icons/sucsess.png", "Transparent", "#1152FF00").Show();
+            //new Notification("Client Disconnected", "Evoq disconnected from server 2332 ", "#2686E6", "pack://application:,,,/UI/Assets/Icons/Info.png", "Transparent", "#1100ACFF").Show();
+            //new Notification("Client Disconnected", "Evoq disconnected from server 2332 ", "#E80000", "pack://application:,,,/UI/Assets/Icons/warning.png", "Transparent", "#11FF0000").Show();
+            //new Notification("Client Disconnected", "Evoq disconnected from server 2332 Evoq disconnected from server 2332", "#52FF00", "pack://application:,,,/UI/Assets/Icons/sucsess.png", "Transparent", "#1152FF00").Show();
         }
 
         private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
