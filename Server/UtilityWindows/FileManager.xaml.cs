e3557055c55e8619d23d7215be3555a5eb62d0b9
full_seal <c36177541@gmail.com>
2024-09-09 21:01:12 +0200

    File Manager #1

--- Server/UtilityWindows/FileManager.xaml.cs
+++ Server/UtilityWindows/FileManager.xaml.cs
@@ -1,136 +1,65 @@
-﻿using Server.UtilityWindows.Interface;
-using System.Windows.Controls;
-using System.Globalization;
-using System.Windows.Data;
+﻿using Server.CoreServerFunctionality;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
-using Server.CoreServerFunctionality;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace Server.UtilityWindows
 {
-    public partial class FileManager : Window, IUtilityWindow 
+    /// <summary>
+    /// Interaction logic for FileManager.xaml
+    /// </summary>
+    public partial class FileManager : Window
     {
         private readonly ServerSession _serverSession;
-        private const int MaxItems = 5;
+        public ObservableCollection<YourDataModel> DataItems { get; set; }
 
         public FileManager(ServerSession serverSession)
         {
             InitializeComponent();
             _serverSession = serverSession;
-            LoadFolders();
-            LoadDrives();
-            LoadRecentFiles();
-        }
 
-        private void LoadFolders()
-        {
-            var folders = new List<Folder>
+            DataItems = new ObservableCollection<YourDataModel>
             {
-                new() { ImageSource = @"\UI\Assets\Windows\FileManager\FileManagerDefaultFolderIcon.png", FolderTitle = "Desktop", FileCount = GetFileCount("Desktop") },
-                new() { ImageSource = @"\UI\Assets\Windows\FileManager\FileManagerDownloadsFolderIcon.png", FolderTitle = "Downloads", FileCount = GetFileCount("Downloads") },
-                new() { ImageSource = @"\UI\Assets\Windows\FileManager\FileManagerDocumentsFolderIcon.png", FolderTitle = "Documents", FileCount = GetFileCount("Documents") },
-                new() { ImageSource = @"\UI\Assets\Windows\FileManager\FileManagerPicturesFolderIcon.png", FolderTitle = "Pictures", FileCount = GetFileCount("Pictures") },
-                new() { ImageSource = @"\UI\Assets\Windows\FileManager\FileManagerVideosFolderIcon.png", FolderTitle = "Videos", FileCount = GetFileCount("Videos") },
+                new YourDataModel { Name = "name 1", Size = "1 KB", Modified = new DateTime(2024, 8, 13) },
+                new YourDataModel { Name = "name 2", Size = "2 KB", Modified = new DateTime(2024, 9, 5) },
+                new YourDataModel { Name = "name 3", Size = "3 KB", Modified = new DateTime(2024, 9, 5) },
+                new YourDataModel { Name = "name 4", Size = "4 KB", Modified = new DateTime(2024, 9, 5) },
+                new YourDataModel { Name = "name 5", Size = "5 KB", Modified = new DateTime(2024, 9, 5) },
+                new YourDataModel { Name = "name 6", Size = "6 KB", Modified = new DateTime(2024, 9, 5) },
+                new YourDataModel { Name = "name 7", Size = "7 KB", Modified = new DateTime(2024, 9, 5) },
+                new YourDataModel { Name = "name 8", Size = "8 KB", Modified = new DateTime(2024, 9, 5) },
+                new YourDataModel { Name = "name 8", Size = "8 KB", Modified = new DateTime(2024, 9, 5) },
+                new YourDataModel { Name = "name 8", Size = "8 KB", Modified = new DateTime(2024, 9, 5) },
+                new YourDataModel { Name = "name 8", Size = "8 KB", Modified = new DateTime(2024, 9, 5) },
+                new YourDataModel { Name = "name 8", Size = "8 KB", Modified = new DateTime(2024, 9, 5) },
+                new YourDataModel { Name = "name 8", Size = "8 KB", Modified = new DateTime(2024, 9, 5) },
+                new YourDataModel { Name = "name 8", Size = "8 KB", Modified = new DateTime(2024, 9, 5) },
+                new YourDataModel { Name = "name 8", Size = "8 KB", Modified = new DateTime(2024, 9, 5) },
+                new YourDataModel { Name = "name 8", Size = "8 KB", Modified = new DateTime(2024, 9, 5) },
+                new YourDataModel { Name = "name 8", Size = "8 KB", Modified = new DateTime(2024, 9, 5) }
             };
 
-            for (int i = 0; i < folders.Count; i++)
-            {
-                FolderContainer.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(5, GridUnitType.Pixel) });
-                FolderContainer.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-
-                var folder = folders[i];
-                var contentControl = new ContentControl
-                {
-                    ContentTemplate = (DataTemplate)Resources["FolderTemplate"],
-                    Content = folder
-                };
-
-                Grid.SetColumn(contentControl, i * 2 + 1);
-                FolderContainer.Children.Add(contentControl);
-            }
+            dataGridPorts.ItemsSource = DataItems;
         }
-
-        private void LoadDrives()
+        public class YourDataModel
         {
-            var drives = new List<Drive>
-            {
-                new() { DriveTitle = "Local Disk (C:)", FileSystem = "File System:", Hardware = "Hardware:" },
-                new() { DriveTitle = "Backup (D:)", FileSystem = "File System:", Hardware = "Hardware:" },
-            };
+            public string Name { get; set; }
+            public string Size { get; set; }
+            public DateTime Modified { get; set; }
 
-            foreach (var drive in drives)
-            {
-                var contentControl = new ContentControl
-                {
-                    ContentTemplate = (DataTemplate)Resources["DriveTemplate"],
-                    Content = drive
-                };
-                DriveContainer.Children.Add(contentControl);
-            }
-        }
-
-        private void LoadRecentFiles()
-        {
-            var recentFiles = new List<RecentFile>
-            {
-                new() { FileTypeImage = "Images/PNGIcon.png", FileName = "example1.png", FileType = "PNG file", FileSize = "18 MB", LastModified = "Last Modified 2023-10-01" },
-                new() { FileTypeImage = "Images/WAVIcon.png", FileName = "example2.wav", FileType = "WAV file", FileSize = "1.53 GB", LastModified = "Last Modified 2023-09-15" },
-            };
-
-            foreach (var recentFile in recentFiles)
-            {
-                var contentControl = new ContentControl
-                {
-                    ContentTemplate = (DataTemplate)Resources["RecentFileTemplate"],
-                    Content = recentFile
-                };
-                RecentFilesContainer.Children.Add(contentControl);
-            }
-        }
-
-        private string GetFileCount(string folderName)
-        {
-            // Example: return Directory.GetFiles(folderPath).Length + " files";
-            return "10 files"; // Demo
-        }
-    }
-
-    public class Folder
-    {
-        public string ImageSource { get; set; }
-        public string FolderTitle { get; set; }
-        public string FileCount { get; set; }
-    }
-
-    public class Drive
-    {
-        public string DriveTitle { get; set; }
-        public string FileSystem { get; set; }
-        public string Hardware { get; set; }
-    }
-
-    public class RecentFile
-    {
-        public string FileTypeImage { get; set; }
-        public string FileName { get; set; }
-        public string FileType { get; set; }
-        public string FileSize { get; set; }
-        public string LastModified { get; set; }
-    }
-
-    public class PercentageConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is double windowWidth)
-            {
-                return windowWidth * 0.4;
-            }
-            return 0;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
+            // Combined property for displaying text in the DataGrid
+            public string DisplayText => $"Type {Name} Size {Size} Modified {Modified:dd MMM yyyy}";
         }
     }
 }
